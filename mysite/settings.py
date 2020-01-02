b382a1e7bf9adaeb815d43715c85b7327350a5e1
Your Name <you@example.com>
2020-01-02 19:52:24 +0100

    bsdbhrh

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r!w0qji+!v_&5d+uzv0k0(4=y5j^yej-i&@+081vl$ago(s84v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite',
 ]
 
 MIDDLEWARE = [
