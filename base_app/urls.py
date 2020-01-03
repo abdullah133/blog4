203741eac74ad54b9cdea5b476952361508455e9
Your Name <you@example.com>
2020-01-03 18:45:50 +0100

    serbhdrhbrf

--- base_app/urls.py
+++ base_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 
+app_name = 'base_app'
+
 urlpatterns = [
     path('', views.homeview, name='home_page'),
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homeview(request):
+    return render(request, 'base_app/home.html', {})
